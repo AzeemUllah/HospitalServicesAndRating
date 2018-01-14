fe89c4f1c110ea4bb5dfeeebaa9ba9f39eaeec00
Azeem Ullah <azeemullah@gmx.com>
2018-01-14 17:59:56 +0500

    Day Three - Take Two

--- app/src/main/java/hospital/hospital/Chat.java
+++ app/src/main/java/hospital/hospital/Chat.java
@@ -1,10 +1,13 @@
 package hospital.hospital;
 
+import android.support.design.widget.BottomSheetDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,10 +15,16 @@ import java.util.List;
 import hospital.hospital.ChatResources.ChatAdapter;
 import hospital.hospital.ChatResources.ChatData;
 
-public class Chat extends AppCompatActivity {
+public class Chat extends AppCompatActivity implements View.OnClickListener {
     private List<ChatData> chatList = new ArrayList<>();
     private RecyclerView recyclerView;
     private ChatAdapter mAdapter;
+
+    private ImageButton mMoreOptionsImgBtn;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +38,48 @@ public class Chat extends AppCompatActivity {
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
 
+        mMoreOptionsImgBtn = (ImageButton) findViewById(R.id.moreOptionsImgBtn);
+        mMoreOptionsImgBtn.setOnClickListener(this);
+
+
+
+
         prepareMovieData();
     }
 
+    @Override
+    public void onClick(View v){
+        switch (v.getId()) {
+            case R.id.moreOptionsImgBtn:
+                showBottomSheetDialog();
+                break;
+//            case R.id.closeFrag:
+//                Toast.makeText(this,
+//                        "Button is clicked!", Toast.LENGTH_LONG).show();
+//                break;
+            default:
+                break;
+        }
+    }
 
+    public void showBottomSheetDialog() {
+        // 2 diffrent fragments one for user and other for facility
+        View view =  getLayoutInflater().inflate(R.layout.fragment_chat_bottom_sheet_user_side, null);
+        BottomSheetDialog dialog = new BottomSheetDialog(this,R.style.NewDialog);
+
+
+//        Button mCloseFrag = (Button) view.findViewById(R.id.closeFrag);
+//        mCloseFrag.setOnClickListener(this);
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.setContentView(view);
+        dialog.show();
+    }
+
+//    @OnClick(R.id.btn_bottom_sheet_dialog_fragment)
+//    public void showBottomSheetDialogFragment() {
+//        fragment_chat_bottom_sheet_facility_side bottomSheetFragment = new fragment_chat_bottom_sheet_facility_side();
+//        bottomSheetFragment.show(getSupportFragmentManager(), bottomSheetFragment.getTag());
+//    }
 
     private void prepareMovieData() {
         chatList.add(new ChatData("","","",""));
---
+++ app/src/main/java/hospital/hospital/FacilityProfile.java
@@ -0,0 +1,13 @@
+package hospital.hospital;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FacilityProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facility_profile);
+    }
+}
